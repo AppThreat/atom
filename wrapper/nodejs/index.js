f48d1731d6b986af5a6c785a5a9e7dde9f764ec5
Prabhu Subramanian <prabhu@appthreat.com>
2023-09-14 16:13:09 +0100

    Restore custom logging configuration
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- wrapper/nodejs/index.js
+++ wrapper/nodejs/index.js
@@ -13,12 +13,14 @@ if (!url.startsWith("file://")) {
 }
 const dirName = import.meta ? dirname(fileURLToPath(url)) : __dirname;
 
+export const LOG4J_CONFIG = join(dirName, "plugins", "log4j2.xml");
 export const ATOM_HOME = join(dirName, "plugins");
 export const APP_LIB_DIR = join(ATOM_HOME, "lib");
 const freeMemoryGB = Math.max(Math.floor(freemem() / 1024 / 1024 / 1024), 4);
-export const JVM_ARGS =
-  "-XX:+UseG1GC -XX:+UseStringDeduplication";
-export const JAVA_OPTS = `${process.env.JAVA_OPTS || ""} -Xmx${freeMemoryGB}G ${JVM_ARGS}`;
+export const JVM_ARGS = "-XX:+UseG1GC -XX:+UseStringDeduplication";
+export const JAVA_OPTS = `${
+  process.env.JAVA_OPTS || ""
+} -Xmx${freeMemoryGB}G ${JVM_ARGS}`;
 export const APP_MAIN_CLASS = "io.appthreat.atom.Atom";
 export const ATOM_VERSION = "1.2.0";
 export const APP_CLASSPATH = join(
@@ -55,6 +57,7 @@ export const executeAtom = (atomArgs) => {
     .concat([
       "-cp",
       atomLibs.join(delimiter),
+      `-Dlog4j.configurationFile=${LOG4J_CONFIG}`,
       APP_MAIN_CLASS,
       ...atomArgs
     ]);
