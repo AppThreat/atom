fbe510fa836958f675c2252b3e72c3a87de5cfd1
Prabhu Subramanian <prabhu@appthreat.com>
2024-01-09 14:11:04 +0000

    Update action
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- wrapper/nodejs/build.ps1
+++ wrapper/nodejs/build.ps1
@@ -1,6 +1,7 @@
 copy ..\..\target\atom.zip plugins\
 Expand-Archive -Path ..\..\target\atom.zip -DestinationPath plugins\ -Force
-Remove-Item plugins\atom.zip
+Remove-Item -Force plugins\atom.zip
+
 php --php-ini php.ini -r "copy('http://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php --php-ini php.ini composer-setup.php
@@ -10,6 +11,6 @@ php --php-ini php.ini composer.phar require nikic/php-parser:4.18.0 --ignore-pla
 
 npm install
 
-rm composer.phar
-rm composer.json
-rm composer.lock
+Remove-Item -Force composer.phar
+Remove-Item -Force composer.json
+Remove-Item -Force composer.lock
