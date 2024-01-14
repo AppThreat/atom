d2a520f5e46666d6c2f66078f03189559a10f3ee
Prabhu Subramanian <prabhu@appthreat.com>
2024-01-14 15:25:31 +0000

    native-image config for more languages
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- ci/trace-native-image.sh
+++ ci/trace-native-image.sh
@@ -10,6 +10,6 @@
 # sdk use java 21.0.1-graalce
 
 sbt clean stage
-./atom.sh -J-agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image reachables -l $1 -o /tmp/app.atom -s /tmp/reachables.slices.json $2
+./atom.sh -J-agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image reachables -l $1 -o /tmp/app.atom -s /tmp/reachables.slices.json $2
 
 # bash ci/native-image.sh
