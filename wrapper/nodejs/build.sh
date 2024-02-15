cdf5d07fe40dea94091abadaac5c0e37c0abbc60
Prabhu Subramanian <prabhu@appthreat.com>
2024-02-15 17:07:20 +0000

    Update chen and cdt. Use nospaces option for slices json output
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- wrapper/nodejs/build.sh
+++ wrapper/nodejs/build.sh
@@ -8,7 +8,6 @@ else
     echo "Build the atom project using 'sbt createDistribution' before running this script"
 fi
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 export COMPOSER_VENDOR_DIR=plugins
