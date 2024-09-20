7a3b78a0e37f84fa8ca069367cb665896b024ccc
prabhu <prabhu@appthreat.com>
2024-09-20 23:29:43 +0100

    Remove jackson data-bind (#158)
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- ci/trace-native-image.sh
+++ ci/trace-native-image.sh
@@ -7,9 +7,9 @@
 # bash ci/trace-native-image.sh py <file path>
 # bash ci/trace-native-image.sh c <file path>
 
-# sdk use java 21.0.1-graalce
+# sdk use java 21.0.2-graalce
 
-sbt clean stage
+# sbt clean stage
 ./atom.sh -J-agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image reachables -l $1 -o /tmp/app.atom -s /tmp/reachables.slices.json $2
 
 # bash ci/native-image.sh
