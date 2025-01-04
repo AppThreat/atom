1ca11288d6e46087973e1d1d71be38a882988356
Aryan Rajoria <aryanrajoria1003@gmail.com>
2025-01-03 20:42:01 -0500

    change bottom banner
    Signed-off-by: Aryan Rajoria <aryanrajoria1003@gmail.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -109,7 +109,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} AppThreat, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
