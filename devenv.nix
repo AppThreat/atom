804782efaf4526c191497b7f80bdbcd0ebb08eb6
prabhu <7842+prabhu@users.noreply.github.com>
2025-05-04 10:26:49 +0100

    Setup devenv.sh (#201)
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

---
+++ devenv.nix
@@ -0,0 +1,89 @@
+{ pkgs, lib, inputs, config, ... }: {
+  # Language-specific topions
+  options = {
+    profile = lib.mkOption {
+      type = lib.types.enum [ "android" "ruby" "php" "c" "cplusplus" "basic" ];
+      default = "basic";
+      description = "Development profile to use";
+    };
+  };
+  config = {
+      android = {
+        enable = lib.mkIf (config.profile == "android") true;
+      };
+      languages = {
+        python = {
+          enable = true;
+          venv.enable = true;
+          version = "3.12";
+        };
+        javascript = {
+          enable = true;
+          package = pkgs.nodejs_23;
+          npm.enable = true;
+        };
+        java = {
+          enable = true;
+          jdk.package = pkgs.jdk23;
+        };
+        ruby = {
+          enable = lib.mkIf (config.profile == "ruby") true;
+          version = "3.4.3";
+        };
+        c = {
+          enable = lib.mkIf (config.profile == "c") true;
+        };
+        cplusplus = {
+          enable = lib.mkIf (config.profile == "cplusplus") true;
+        };
+        scala = {
+          enable = true;
+          sbt.enable = true;
+        };
+        php = {
+          enable = lib.mkIf (config.profile == "php") true;
+          extensions = [
+            "openssl"
+            "zip"
+          ];
+          packages = {
+            composer = pkgs.phpPackages.composer;
+          };
+        };
+      };
+      packages = [
+        pkgs.nodejs_23
+        pkgs.python312Full
+        (pkgs.python312.withPackages (ps: [ ps.pip ps.setuptools ]))
+        pkgs.sbt
+        pkgs.php
+      ];
+      # Useful features
+      devcontainer.enable = true;
+      difftastic.enable = true;
+      # Compile atom and plugins
+      # Setup the latest cdxgen
+      enterShell = ''
+        set -e
+        mkdir -p /tmp/empty-cache
+        export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8 --enable-native-access=ALL-UNNAMED"
+        sudo npm install --cache /tmp/empty-cache --prefer-online -g @cyclonedx/cdxgen
+        cdxgen --version
+        python3 --version
+        python3 -m pip install atom-tools
+        sbt clean stage createDistribution publishLocal
+        cd wrapper/nodejs
+        bash build.sh
+        sudo npm install --cache /tmp/empty-cache --prefer-online -g .
+        cd ../..
+        which atom
+        which astgen
+        which phpastgen
+        which rbastgen
+        which scalasem
+        which atom-tools
+      '';
+
+      # Tasks
+  };
+}
--- wrapper/nodejs/build.sh
+++ wrapper/nodejs/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 rm -rf plugins/bin plugins/lib
 if [ -e "../../target/atom.zip" ]; then
     cp -rf ../../target/atom.zip plugins/
@@ -7,18 +8,25 @@ if [ -e "../../target/atom.zip" ]; then
 else
     echo "Build the atom project using 'sbt createDistribution' before running this script"
 fi
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-export COMPOSER_VENDOR_DIR=plugins
-php composer.phar require nikic/php-parser:4.18.0 --ignore-platform-reqs --optimize-autoloader
 
-cd plugins/rubyastgen
-bash setup.sh
-cd ../..
+if command -v php >/dev/null 2>&1; then
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php composer-setup.php
+  php -r "unlink('composer-setup.php');"
+  export COMPOSER_VENDOR_DIR=plugins
+  php composer.phar require nikic/php-parser:4.18.0 --ignore-platform-reqs --optimize-autoloader
+  rm composer.phar composer.json composer.lock
+else
+  echo "PHP plugins not built."
+fi
 
-rm plugins/bin/racc plugins/bin/ruby-parse plugins/bin/ruby-rewrite
+if command -v ruby >/dev/null 2>&1 && command -v bundle >/dev/null 2>&1; then
+  cd plugins/rubyastgen
+  bash setup.sh
+  cd ../..
+  rm plugins/bin/racc plugins/bin/ruby-parse plugins/bin/ruby-rewrite
+else
+  echo "Ruby plugins not built."
+fi
 
 npm ci
-
-rm composer.phar composer.json composer.lock
