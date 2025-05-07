b7699f18c7de1cbfc248ab740ccc348b4805f053
Prabhu Subramanian <prabhu@appthreat.com>
2025-05-07 15:59:29 +0100

    Switch to mandrel for native builds. Helpful message for empty reachable slices
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- ci/native-image.sh
+++ ci/native-image.sh
@@ -10,7 +10,8 @@ if [ -f "target/graalvm-native-image/atom" ]; then
     chmod +x target/graalvm-native-image/atom
     target/graalvm-native-image/atom --help
     echo "atom native-image was built successfully."
-    echo "Using Oracle GraalVM 23? Adhere to the terms of the license - https://www.oracle.com/downloads/licenses/graal-free-license.html"
+    echo "Mandrel-based builds are released under version 2 of the GNU General Public License with the “Classpath” Exception - https://github.com/graalvm/mandrel/blob/default/LICENSE"
+    echo "Using Oracle GraalVM to build this image? Adhere to the terms of the license - https://www.oracle.com/downloads/licenses/graal-free-license.html"
 else
     echo "atom native-image was not built correctly. Check if you have Oracle GraalVM 23 installed."
 fi
