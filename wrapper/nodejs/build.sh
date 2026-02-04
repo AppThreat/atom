dd634225d21cf02148dcd9a2bf251e2dd30d5ad8
Prabhu Subramanian <prabhu@appthreat.com>
2026-02-04 01:34:08 +0100

    Update to ruby 4.0.1
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- wrapper/nodejs/build.sh
+++ wrapper/nodejs/build.sh
@@ -6,7 +6,11 @@ if [ -e "../../target/atom.zip" ]; then
     unzip -q plugins/atom.zip -d plugins
     rm plugins/atom.zip
 else
-    echo "Build the atom project using 'sbt createDistribution' before running this script"
+    echo "Build the atom project using 'sbt createDistribution' before running this script."
 fi
-
+if [ -e "../../*.cdx.json" ]; then
+  cp "../../*.cdx.json" plugins/
+else
+  echo "Generate an SBOM with cdxgen before running this script for inclusion."
+fi
 npm ci
